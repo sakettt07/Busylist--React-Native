89dbf73a7d7388477f68228fd4d1bc9028f7ea7f
saket <saket@saketgupta.com>
2026-01-31 23:56:37 +0530

    Update BusinessDetail component to log business details and change axios baseURL for API

--- app/business-detail/index.tsx
+++ app/business-detail/index.tsx
@@ -4,6 +4,7 @@ import { useLocalSearchParams } from 'expo-router'
 
 const BusinessDetail = () => {
     const { business } = useLocalSearchParams();
+    console.log("THis is my business----", business);
     return (
         <View>
             <Text>index</Text>
